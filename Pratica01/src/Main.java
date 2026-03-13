89e9990035cef3f8d1641277a0e1f1f8cfab2ddf
Ana Júlia Sena <ana.oliveira.1602783@sga.pucminas.br>
2026-03-12 21:35:45 -0300

    Questão completa e testada

--- Pratica01/src/Main.java
+++ Pratica01/src/Main.java
@@ -1,3 +1,20 @@
 void main() {
-    
+    Scanner scanner = new Scanner(System.in);
+    Pessoa pessoa1 = new Pessoa();
+
+    System.out.println("Insira seu nome: ");
+    pessoa1.setNome(scanner.nextLine());
+
+    System.out.println("Insira seu sobrenome: ");
+    pessoa1.setSobrenome(scanner.nextLine());
+
+    System.out.println("Insira sua idade, altura e peso: ");
+    pessoa1.setIdade(scanner.nextInt());
+    pessoa1.setAltura(scanner.nextDouble());
+    pessoa1.setPeso(scanner.nextDouble());
+
+    pessoa1.CalculaIMC();
+    System.out.println("Seu IMC e: " + pessoa1.getIMC());
+    pessoa1.InformaObesidade();
+
 }
